4aeda907f20e70fb8b498a6e5bfdda39dbd1a0e2
elendi2003 <elendi2003@hotmail.com>
2018-07-24 21:45:06 +0300

    Добавил конструктор в класс ServerGUI и инициализировал его соответствующими полями.

--- src/Chat/server/gui/ServerGUI.java
+++ src/Chat/server/gui/ServerGUI.java
@@ -1,11 +1,29 @@
 package Chat.server.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ServerGUI extends JFrame{
+
+    private static final int POS_X = 200;
+    private static final int POS_Y = 200;
+    private static final int WIDTH = 300;
+    private static final int HEIGHT = 100;
+
     public static void main(String[] args){
         SwingUtilities.invokeLater(() -> {
             new ServerGUI();
         });
+    }
+
+    ServerGUI(){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBounds(POS_X, POS_Y, WIDTH, HEIGHT);
+        setResizable(false);
+        setTitle("Chat Server");
+        setAlwaysOnTop(true);// установка окна поверх всех окон
+        setLayout(new GridLayout(1,2));
+
+        setVisible(true);
     }
 }
