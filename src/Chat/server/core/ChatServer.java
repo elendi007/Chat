c45c6660c11354f103485d8a1506c49dd57a5114
elendi2003 <elendi2003@hotmail.com>
2018-07-24 18:50:09 +0300

    Добавил в класс ChatServer два метода: start(int port) и stop

--- src/Chat/server/core/ChatServer.java
+++ src/Chat/server/core/ChatServer.java
@@ -1,4 +1,11 @@
 package Chat.server.core;
 
 public class ChatServer {
+    public void start(int port){
+        System.out.println("Server started at port: " + port);
+    }
+
+    public void stop(){
+        System.out.println("Server stoped");
+    }
 }
