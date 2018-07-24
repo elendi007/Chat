df59fed07ad905cd6a9c3dedc18cf4254af373e1
elendi2003 <elendi2003@hotmail.com>
2018-07-24 22:25:39 +0300

    Добавил в класс ServerGUI две кнопки, а так же обработчики событий нажатия этих кнопок с помощью метода addActionListener, который в качестве аргумента принимает объект анонимного класса, реализующего интерфейс ActionListener.

--- src/Chat/server/gui/ServerGUI.java
+++ src/Chat/server/gui/ServerGUI.java
@@ -2,6 +2,8 @@ package Chat.server.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ServerGUI extends JFrame{
 
@@ -9,6 +11,9 @@ public class ServerGUI extends JFrame{
     private static final int POS_Y = 200;
     private static final int WIDTH = 300;
     private static final int HEIGHT = 100;
+
+    private static final JButton btnStart = new JButton("Start");
+    private static final JButton btnStop = new JButton("Stop");
 
     public static void main(String[] args){
         SwingUtilities.invokeLater(() -> {
@@ -23,6 +28,22 @@ public class ServerGUI extends JFrame{
         setTitle("Chat Server");
         setAlwaysOnTop(true);// установка окна поверх всех окон
         setLayout(new GridLayout(1,2));
+
+        btnStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Button start press");
+            }
+        });
+        add(btnStart);
+
+        btnStop.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Button stop press");
+            }
+        });
+        add(btnStop);
 
         setVisible(true);
     }
